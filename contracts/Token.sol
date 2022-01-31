1fa3c57da0a14cda6e5b2c2618fab43b051b70ac
spandan114 <spandanj685@gmail.com>
2022-02-01 00:22:23 +0530

    Deploy & transfer token testcases added

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,4 +6,20 @@ import "hardhat/console.sol";
 contract Token {
    string public name = "BrownieToken";
    string public symbol = "BW";
+   uint256 public decimals = 18;
+   uint256 public totalSupply ;
+
+   mapping(address => uint256) public balanceOf;
+
+constructor(){
+   totalSupply = 1000000 * (10**decimals);
+   balanceOf[msg.sender] = totalSupply;
+}
+
+function transfer(address _to,uint256 _value)public {
+   require(balanceOf[msg.sender] >= _value,"You dont have enough tokens");
+   balanceOf[msg.sender] -= _value;
+   balanceOf[_to] += _value;
+}
+
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,51 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Token",  async () => {
-    // const accounts = await ethers.getSigners();  
-    // for (const account of accounts) {
-    //     console.log(account.address);
-    //   }
+describe("Token", async () => {
 
-      const Token = await ethers.getContractFactory("Token"); //create instance
-      const hardhatToken = await Token.deploy();//Deploy contract
-      describe('Deployment', () => {
-      it("Name should be SJ", async ()=>{
-        expect(await hardhatToken.name()).to.equal('SJ')
-      })
+  var owner;
+  var address;
+  const name = "BrownieToken";
+  const symbol = "BW";
+  const totalSupply = '1000000000000000000000000';
+  var tokenContract
+
+  beforeEach(async () => {
+    [owner,address1,...address] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token"); //create instance
+    tokenContract = await Token.deploy(); //Deploy contract
+  });
+
+ 
+  describe("Deployment", () => {
+
+    it(`Name should be ${name}`, async () => {
+      expect(await tokenContract.name()).to.equal(name);
+    });
+    it(`Symbol should be ${symbol}`, async () => {
+      expect(await tokenContract.symbol()).to.equal(symbol);
+    });
+    it(`Total supply should be ${totalSupply}`, async () => {
+      const supply = await tokenContract.totalSupply()
+      expect(supply.toString()).to.equal(totalSupply);
+    });
+    it(`Assign total supply to deployer`, async () => {
+      const ownerToken = await tokenContract.balanceOf(owner.address)
+      const supply = await tokenContract.totalSupply()
+      expect(ownerToken.toString()).to.equal(supply.toString());
+    });
+  });
+
+  describe("Transfer", () => {
+    it("transfer token",async()=>{
+      await tokenContract.transfer(address1.address,10,{from:owner.address})
+      const ownerToken = await tokenContract.balanceOf(owner.address)
+      const address1Token = await tokenContract.balanceOf(address1.address)
+      expect(BigInt(ownerToken).toString()).to.equal("999999999999999999999990")
+      console.log("phase1")
+      expect(address1Token.toString()).to.equal("10")
+      
     })
+  })
 
-})
+});
