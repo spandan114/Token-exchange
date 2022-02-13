e68f26ed4ccaf7de335d9cac106b195bf3dd2a12
spandan114 <spandanj685@gmail.com>
2022-02-13 22:40:56 +0530

    Deposite ether

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,6 +11,8 @@ contract Exchange {
 
 address public feeAccount;
 uint256 public feePercent;
+address ETHER = address(0);
+// erc-20 token address => ( my address => no of token)
 mapping(address=>mapping(address=>uint256)) public tokens;
 
 event Deposite(address indexed token, address indexed user, uint256 amount, uint256 balance);
@@ -20,15 +22,26 @@ constructor(address _feeAccount,uint256 _feePercent){
     feePercent = _feePercent;
 }
 
+//FALLBACK : revert if someone bymistake send ether to smartcontract
+fallback() external payable{
+  revert();
+}
+
+function depositeEther() payable public {
+tokens[ETHER][msg.sender] += msg.value;
+emit Deposite(ETHER,msg.sender,msg.value,tokens[ETHER][msg.sender]);
+}
+
 function depositeToken(address _token,uint256 _amount) public {
+  // Reject eth deposite
   // which ERC-20 token ?
   // How much amount ?
   // Send token to this contract
   // Manage Deposite balance
   // emit event
   
-  //TODO: Reject eth deposite... 
-  require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+  require(_token != ETHER,"Ether deposite not allowed");
+  require(Token(_token).transferFrom(msg.sender, address(this), _amount),"You dont have enough tokens");
   tokens[_token][msg.sender] += _amount;
   emit Deposite(_token,msg.sender,_amount,tokens[_token][msg.sender]);
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -8,6 +8,7 @@ describe("Exchange", async () => {
   var address2;
   var address4;
   var address5;
+  var ETHER = "0x0000000000000000000000000000000000000000";
 
   var feeAccount;
   var feePercent = 10;
@@ -35,8 +36,7 @@ describe("Exchange", async () => {
     });
   });
 
-  describe("Deposite token", () => {
-
+  describe("Deposits token", () => {
     it(`Track token deposited`, async () => {
       await tokenContract.connect(owner).approve(exchangeContract.address, 1000);
       const depositedToken = await exchangeContract.connect(owner).depositeToken(tokenContract.address, 1000);
@@ -44,20 +44,65 @@ describe("Exchange", async () => {
       const event = await depositedToken.wait();
 
       expect(event.events.length).to.equal(2);
-      expect(event.events[1].event).to.equal('Deposite');
+      expect(event.events[1].event).to.equal("Deposite");
       expect(event.events[1].args.token).to.equal(tokenContract.address);
       expect(event.events[1].args.user).to.equal(owner.address);
       expect(event.events[1].args.amount.toNumber()).to.equal(1000);
       expect(event.events[1].args.balance.toNumber()).to.equal(1000);
-      
 
       //Check contract balance
-      const contractBalance = await tokenContract.balanceOf(exchangeContract.address);
-      expect(contractBalance.toString()).to.equal('1000');
-      //Check token on exchange 
-      const tokenBalance = await exchangeContract.tokens(tokenContract.address,owner.address);
-      expect(tokenBalance.toString()).to.equal('1000');
+      const contractBalance = await tokenContract.balanceOf(
+        exchangeContract.address
+      );
+      expect(contractBalance.toString()).to.equal("1000");
+      //Check token on exchange
+      const tokenBalance = await exchangeContract.tokens(
+        tokenContract.address,
+        owner.address
+      );
+      expect(tokenBalance.toString()).to.equal("1000");
     });
 
+    it(`Reject ether deposited`, async () => {
+      await expect(
+        exchangeContract
+          .connect(address2)
+          .depositeToken(ETHER, 10)
+      ).to.be.revertedWith("Ether deposite not allowed");
+    });
+
+    it(`Failed if user doesn't have enough tokens`, async () => {
+      await expect(
+        exchangeContract
+          .connect(address2)
+          .depositeToken(tokenContract.address, 10)
+      ).to.be.revertedWith("Account balance low");
+    });
   });
+
+  describe("Deposits ether", () => {
+    it(`Track ether deposited`, async () => {
+      const depositeEther = await exchangeContract
+      .connect(address4)
+      .depositeEther({value: ethers.utils.parseEther("1")});
+
+      const event = await depositeEther.wait();
+
+      expect(event.events.length).to.equal(1);
+      expect(event.events[0].event).to.equal("Deposite");
+      expect(event.events[0].args.token).to.equal(ETHER);
+      expect(event.events[0].args.user).to.equal(address4.address);
+      expect(event.events[0].args.amount).to.equal(ethers.utils.parseEther("1"));
+      expect(event.events[0].args.balance).to.equal(ethers.utils.parseEther("1"));
+
+      const etherBalance = await exchangeContract.tokens(
+        ETHER,
+        address4.address
+      );
+
+      expect(etherBalance).to.equal(ethers.utils.parseEther("1"));
+
+    });
+  });
+
 });
