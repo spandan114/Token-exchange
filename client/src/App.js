37b7a17edf2cad441454e5badc23291c56c1184c
spandan114 <spandanj685@gmail.com>
2022-02-21 22:57:47 +0530

    UI setup

--- client/src/App.js
+++ client/src/App.js
@@ -7,32 +7,32 @@ function App() {
     <div className="exchange-container container-fluid mt-3">
       <div className="d-flex flex-row">
         <div className="card-sm">
-          <div className="card ">
+          <div className="card height-half">
             <h5 className="card-header">Balance</h5>
             <div className="card-body"></div>
           </div>
-          <div className="card ">
+          <div className="card height-half">
             <h5 className="card-header">New Order</h5>
             <div className="card-body"></div>
           </div>
         </div>
         <div className="card-sm">
-          <div className="card ">
+          <div className="card height-full">
             <h5 className="card-header">Orders</h5>
             <div className="card-body"></div>
           </div>
         </div>
         <div className="card-lg">
-          <div className="card p-2">
+          <div className="card p-2 height-half">
             <div className="card-body"></div>
           </div>
-          <div className="card">
+          <div className="card height-half">
             <h5 className="card-header">My Transactions</h5>
             <div className="card-body"></div>
           </div>
         </div>
         <div className="card-sm">
-          <div className="card ">
+          <div className="card height-full">
             <h5 className="card-header">Trades</h5>
             <div className="card-body"></div>
           </div>
