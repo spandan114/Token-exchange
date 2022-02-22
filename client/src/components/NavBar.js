78727e3fffb571f3e9950161e0088d319bcd69d9
spandan114 <spandanj685@gmail.com>
2022-02-23 00:24:44 +0530

    Load contract data & store it in redux

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+
+  const account = useSelector((state) => state.web3Reducer.account);
+
   return (
     <nav className="navbar navbar-expand-lg pl-md-5 pl-md-5 ">
       <a className="navbar-brand" href="#">
@@ -22,7 +26,7 @@ const NavBar = () => {
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <p className="nav-link" href="#">
-              Connect wallet
+              {account?account:"Connect wallet"}
             </p>
           </li>
         </ul>
--- client/src/redux/interactions.js
+++ client/src/redux/interactions.js
@@ -1,34 +1,47 @@
-import { accountLoaded, exchangeContractLoaded, tokenContractLoaded, web3Loaded } from "./actions";
-import Web3 from 'web3';
-import ExchangeContract from "../artifacts/contracts/Exchange.sol/Exchange.json"
-import TokenContract from "../artifacts/contracts/Token.sol/Token.json"
+import {
+  accountLoaded,
+  exchangeContractLoaded,
+  tokenContractLoaded,
+  web3Loaded,
+} from "./actions";
+import Web3 from "web3";
+import ExchangeContract from "../artifacts/contracts/Exchange.sol/Exchange.json";
+import TokenContract from "../artifacts/contracts/Token.sol/Token.json";
 
 export const loadWeb3 = async (dispatch) => {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    dispatch(web3Loaded(web3))
-    return web3
-}
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+  dispatch(web3Loaded(web3));
+  return web3;
+};
 
-export const loadAccount = async (web3,dispatch) => {
-    const account = await web3.eth.getAccounts()
-    dispatch(accountLoaded(account))
-    return account
-}
+export const loadAccount = async (web3, dispatch) => {
+  const account = await web3.eth.getAccounts();
+  dispatch(accountLoaded(account));
+  return account;
+};
 
-export const loadTokenContract = async (web3,dispatch) => {
+export const loadTokenContract = async (web3, dispatch) => {
+  try {
     const tokenContract = new web3.eth.Contract(
-        TokenContract.abi,
-        "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-      );
-    dispatch(tokenContractLoaded(tokenContract))
-    return null
-}
+      TokenContract.abi,
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    );
+    dispatch(tokenContractLoaded(tokenContract));
+    return null;
+  } catch (error) {
+    alert("Something went wrong token contract not loaded !");
+  }
+};
 
-export const loadExchangeContract = async (web3,dispatch) => {
+export const loadExchangeContract = async (web3, dispatch) => {
+  try {
     const tokenContract = new web3.eth.Contract(
-        ExchangeContract.abi,
-        "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-      );
-    dispatch(exchangeContractLoaded(tokenContract))
-    return null
-}
+      ExchangeContract.abi,
+      "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+    );
+    dispatch(exchangeContractLoaded(tokenContract));
+    return null;
+  } catch (error) {
+    alert("Something went wrong token contract not loaded !");
+  }
+};
